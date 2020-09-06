166fb1a254794c45ee8c4e99f8e2ddfc6d4ad347
Subwayqueen <70745660+Subwayqueen@users.noreply.github.com>
2020-09-06 11:26:10 +0100

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -92,11 +92,11 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>○ File :</b> <code>{download.name()}</code> \n<b>○ Size :</b> <b>{download.size()}</b>"
+            msg += f"<b>○ File :</b> <code>{download.name()}</code>"
             msg += f"\n<b>○ Status :</b> <b>{download.status()}</b>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)}</code> <b>{download.progress()}</b>" \
-                       f"\n<b>○ Downloaded :</b> <b>{get_readable_file_size(download.processed_bytes())}</b>" \
+                       f"\n<b>○ Downloaded :</b> <b>⪻ {get_readable_file_size(download.processed_bytes())} ⪼</b> <code>Of</code > <b>『 {download.size()} 』</b>" \
                        f"\n<b>○ Speed :</b> {download.speed()} | <b>○ ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
